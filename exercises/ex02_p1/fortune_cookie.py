f89cd74fe1a6475e3d2a745921dd8fa683b70cf2
Anisa <anamarie@email.unc.edu>
2021-02-12 15:05:30 -0500

    ex02_p1 final

--- exercises/ex02_p1/fortune_cookie.py
+++ exercises/ex02_p1/fortune_cookie.py
@@ -1,21 +1,34 @@
 """Fortune cookie exercise redux as a structured program."""
 
 from random import randint
-
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730212606"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     print("Your fortune cookie says...")
-    # TODO 2: Print the result of calling your fortune_cookie function.
+    # TODO 2: print(fortune_cookie)
+    print(fortune_cookie())
     print("Now, go spread positive vibes!")
 
 
-# TODO 1: Define your fortune_cookie function here.
-
-
+# TODO 1: def fortune_cookie() -> str:
+def fortune_cookie() -> str:
+    """The fortune cookie's second line."""
+    your_fortune: int = randint(10, 40)
+    if your_fortune < 10: 
+        if your_fortune < 5:
+            return("You're going to struggle before rising back up.")
+        else: 
+            return("You're going to have one wonderful, perfect day.")
+    else: 
+        if your_fortune <= 20:
+            return("You'll meet the love of your life on a Tuesday evening.")
+        else: 
+            return("You'll see someone you've forgotten about.")
 # Python Idiom for "starting" the program when run as a module.
 # The special dunder variable __name__ will be "__main__" when run as module. 
+
+
 if __name__ == "__main__":
     main()
--- exercises/ex02_p1/tar_heels.py
+++ exercises/ex02_p1/tar_heels.py
@@ -1,15 +1,28 @@
 """Tar Heels exercise redux as a structured program."""
 
-__author__ = "YOUR 9-DIGIT PID"
+__author__ = "730212606"
 
 
 def main() -> None:
     """The entrypoint of the program, when run as a module."""
     choice: int = int(input("Enter an int: "))
-    # TODO 2: Print the response of calling the tar_heels function here.
+    # TODO 2: print(tar_heels)
+    print(tar_heels(choice))
 
 
-# TODO 1: Define the tar_heels function, and its logic, here.
+# TODO 1: def tar_heels() -> str
+def tar_heels(choice: int) -> str:
+    """The program that determines if a number is divisible by 2 and/or 7."""
+    if choice % 2 == 0 and choice % 7 == 0: 
+        return('TAR HEELS')
+    else: 
+        if choice % 2 == 0:
+            return('TAR')
+        else: 
+            if choice % 7 == 0: 
+                return('HEELS')
+            else: 
+                return('CAROLINA')
 
 
 if __name__ == "__main__":
